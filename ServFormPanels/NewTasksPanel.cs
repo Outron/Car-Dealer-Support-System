5f5a3620c73c3facdaa6db9c6766ff29697661ac
Damian Osika <djdamian20011@gmail.com>
2024-01-11 13:50:34 +0100

    Utworzony panel serwisanta

---
+++ ServFormPanels/NewTasksPanel.cs
@@ -0,0 +1,28 @@
+﻿using CarDealerSupportSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarDealerSupportSystem.SellerFormPanels
+{
+    public partial class NewTasksPanel : Form
+    {
+        private readonly salon_samochodowyContext db = new salon_samochodowyContext();
+        public NewTasksPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void OrdersPanel_Load(object sender, EventArgs e)
+        {
+            var orders = db.Zamowienia.ToList();
+            OrdersGridView.DataSource = orders;
+        }
+    }
+}
